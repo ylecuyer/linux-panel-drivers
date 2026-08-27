e3d53e649085e3465eedebd46e80f15e0ab1fede
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2026-08-27 22:24:37 +0200

    config/dtb: Add asus-x00id

---
+++ config/asus-x00id.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+OPTIONS=()
+PANELS=(
+	[sim_video]="asus,x00i-sim-video"
+	[truly_1080p_video]="asus,x00i-truly-1080p"
+	[r69006_1080p_video]="asus,x00i-r69006-1080p"
+	[hx8394f_720p_video]="asus,x00i-hx8394f-720p"
+	[truly_720p_video]="asus,x00i-truly-720p"
+	[truly_wuxga_video]="asus,x00i-truly-wuxga"
+	[lead_fl10802_fwvga_video]="asus,x00i-lead-fl10802-fwvga"
+	[ili9885_fhd_video_dj]="asus,x00i-ili9885-fhd-video-dj"
+	[ili9881c_fhd_video_dj]="asus,x00i-ili9881c-fhd-video-dj"
+)
